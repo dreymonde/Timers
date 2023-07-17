67e5ae1d2b7aeb5f2c36287af97562f0c287522e
Oleg Dreyman <dreymonde@me.com>
2023-07-17 22:01:00 +0200

    first version

--- Sources/Timers/Timers.swift
+++ Sources/Timers/Timers.swift
@@ -1,6 +1,155 @@
-public struct Timers {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+public final class Timers {
+    
+    internal var timers: [Foundation.Timer] = []
+    
+    public init() { }
+    
+    public func clear() {
+        for timer in timers {
+            timer.invalidate()
+        }
+        timers = []
+    }
+    
+    deinit {
+        clear()
+    }
+}
+
+// MARK: - Core Functions
+
+extension Timers {
+    public func manuallyAddTimer(
+        runLoop: RunLoop = .main,
+        runLoopMode: RunLoop.Mode = .common,
+        timer: Timer
+    ) {
+        runLoop.add(timer, forMode: runLoopMode)
+        timers.append(timer)
+    }
+
+    public typealias TimerBlock = (Timer) -> Void
+    
+    public func addCustomTimer<TargetType: AnyObject>(
+        withTarget target: TargetType,
+        handler: @escaping (TargetType, Timer) -> Void,
+        runLoop: RunLoop = .main,
+        runLoopMode: RunLoop.Mode = .common,
+        makeTimer: @escaping (_ timerBlock: @escaping TimerBlock) -> Timer
+    ) {
+        let newTimer = makeTimer { [weak target] (timer) in
+            if let target {
+                handler(target, timer)
+            }
+        }
+        manuallyAddTimer(
+            runLoop: runLoop,
+            runLoopMode: runLoopMode,
+            timer: newTimer
+        )
+    }
+}
+
+// MARK: - Public Helper Functions
+
+extension Timers {
+    public func addRepeating<TargetType: AnyObject>(
+        timeInterval: TimeInterval,
+        tolerance: TimeInterval = 0,
+        withTarget target: TargetType,
+        handler: @escaping (TargetType, Timer) -> Void
+    ) {
+        addCustomTimer(withTarget: target, handler: handler) { timerBlock in
+            let timer = Timer(timeInterval: timeInterval, repeats: true, block: timerBlock)
+            timer.tolerance = tolerance
+            return timer
+        }
+    }
+
+    public func addRepeating<TargetType: AnyObject>(
+        timeInterval: TimeInterval,
+        tolerance: TimeInterval = 0,
+        withTarget target: TargetType,
+        handler: @escaping (TargetType) -> Void
+    ) {
+        addRepeating(
+            timeInterval: timeInterval,
+            tolerance: tolerance,
+            withTarget: target,
+            handler: { target, _ in handler(target) }
+        )
+    }
+}
+
+extension Timers {
+    public func addRepeating<TargetType: AnyObject>(
+        initiallyFireAt fireAt: Date,
+        thenRepeatWithInterval timeInterval: TimeInterval,
+        tolerance: TimeInterval = 0,
+        withTarget target: TargetType,
+        handler: @escaping (TargetType, Timer) -> Void
+    ) {
+        addCustomTimer(
+            withTarget: target,
+            handler: handler,
+            makeTimer: { timerBlock in
+                let timer = Timer(
+                    fire: fireAt,
+                    interval: timeInterval,
+                    repeats: true,
+                    block: timerBlock
+                )
+                timer.tolerance = tolerance
+                return timer
+            }
+        )
+    }
+    
+    public func addRepeating<TargetType: AnyObject>(
+        initiallyFireAt fireAt: Date,
+        thenRepeatWithInterval timeInterval: TimeInterval,
+        tolerance: TimeInterval = 0,
+        withTarget target: TargetType,
+        handler: @escaping (TargetType) -> Void
+    ) {
+        addRepeating(
+            initiallyFireAt: fireAt,
+            thenRepeatWithInterval: timeInterval,
+            tolerance: tolerance,
+            withTarget: target,
+            handler: { target, _ in handler(target) }
+        )
+    }
+}
+
+extension Timers {
+    /// Fires once
+    public func fireAt<TargetType: AnyObject>(
+        _ fireAt: Date,
+        withTarget target: TargetType,
+        handler: @escaping (TargetType) -> Void
+    ) {
+        addCustomTimer(
+            withTarget: target,
+            handler: { target, _ in handler(target) },
+            makeTimer: { timerBlock in
+                Timer(fire: fireAt, interval: 0, repeats: false, block: timerBlock)
+            }
+        )
+    }
+    
+    /// Fires once
+    public func fireAfter<TargetType: AnyObject>(
+        timeInterval: TimeInterval,
+        withTarget target: TargetType,
+        handler: @escaping (TargetType) -> Void
+    ) {
+        fireAt(
+            Date().addingTimeInterval(timeInterval),
+            withTarget: target,
+            handler: handler
+        )
     }
 }
--- Tests/TimersTests/TimersTests.swift
+++ Tests/TimersTests/TimersTests.swift
@@ -2,10 +2,89 @@ import XCTest
 @testable import Timers
 
 final class TimersTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Timers().text, "Hello, World!")
+    
+    var timers: Timers!
+    
+    override func setUp() {
+        super.setUp()
+        timers = Timers()
+    }
+    
+    override func tearDown() {
+        timers.clear()
+        timers = nil
+        super.tearDown()
+    }
+    
+    @objc func dummySelector() {}
+    
+    func testClear() {
+        let dummyTimer = Timer(timeInterval: 10000, target: self, selector: #selector(dummySelector), userInfo: nil, repeats: true)
+        timers.manuallyAddTimer(runLoop: .main, runLoopMode: .common, timer: dummyTimer)
+        XCTAssert(timers.timers.count == 1)
+        timers.clear()
+        XCTAssert(timers.timers.isEmpty)
+    }
+    
+    func testManuallyAddTimer() {
+        let dummyTimer = Timer(timeInterval: 10000, target: self, selector: #selector(dummySelector), userInfo: nil, repeats: true)
+        timers.manuallyAddTimer(runLoop: .main, runLoopMode: .common, timer: dummyTimer)
+        XCTAssert(timers.timers.count == 1)
+    }
+    
+    func testFireAfter() {
+        let expectation = XCTestExpectation(description: "Handler called")
+        timers.fireAfter(timeInterval: 1.0, withTarget: self) { _ in
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 2.0)
+    }
+    
+    func testAddRepeating() {
+        let expectation = XCTestExpectation(description: "Handler called")
+        expectation.expectedFulfillmentCount = 3
+        timers.addRepeating(timeInterval: 0.5, withTarget: self) { _ in
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 2.0)
+    }
+    
+    func testAddRepeatingWithHandler() {
+        let expectation = XCTestExpectation(description: "Handler called")
+        expectation.expectedFulfillmentCount = 3
+        timers.addRepeating(timeInterval: 0.5, withTarget: self) { _, _ in
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 2.0)
+    }
+    
+    func testAddRepeatingWithFireAt() {
+        let expectation = XCTestExpectation(description: "Handler called")
+        timers.addRepeating(initiallyFireAt: Date().addingTimeInterval(1.0), thenRepeatWithInterval: 0.5, withTarget: self) { _, _ in
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 3.0)
+    }
+    
+    func testFireAt() {
+        let expectation = XCTestExpectation(description: "Handler called")
+        timers.fireAt(Date().addingTimeInterval(1.0), withTarget: self) { _ in
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 2.0)
+    }
+    
+    func testFireAtCalledOnce() {
+        let expectation = XCTestExpectation(description: "Handler called")
+        var handlerCallCount = 0
+        timers.fireAt(Date().addingTimeInterval(1.0), withTarget: self) { _ in
+            handlerCallCount += 1
+            if handlerCallCount > 1 {
+                XCTFail("Handler was called more than once.")
+            }
+            expectation.fulfill()
+        }
+        // Extend the wait duration to ensure the timer is not fired more than once
+        wait(for: [expectation], timeout: 3.0)
     }
 }
